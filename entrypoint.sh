ab14860e69a30f4a50e775f289f7118793bbde4f
unknown <sfranckdabryn@gmail.com>
2025-06-13 13:20:41 -0400

    modif work

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "The app is starting ..."
-exec java -jar -Dspring.profiles.active=${SPRING_ACTIVE_PROFILES} "calculator.jar"
+exec java -jar "calculator.jar" --spring.profiles.active=${SPRING_PROFILES_ACTIVE}
